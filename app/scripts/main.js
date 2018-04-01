a4e00609b0a77dda05d6f536dd59d9471cd300a5
anstif <stifurak@gmail.com>
2018-04-01 12:52:25 +0300

    header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,22 @@
+// Minify header when page scroll
 const header = document.querySelector('.header');
 
-window.onscroll = function() {
-  this.oldScroll > this.scrollY
-    ? header.classList.remove('header--hide')
-    : header.classList.add('header--hide');
-
+const headerScroll = function() {
   this.scrollY < header.offsetHeight
     ? header.classList.remove('header--scroll-state')
     : header.classList.add('header--scroll-state');
-
-  this.oldScroll = this.scrollY;
 };
+headerScroll();
+
+window.onscroll = headerScroll;
+
+// Center alignment for submenus
+// Add class for menu with submenu
+const submenus = document.querySelectorAll('.header .nav ul');
+
+submenus.forEach(function(elem) {
+  elem.parentNode.classList.add('nav-child');
+
+  elem.style.left =
+    -Math.abs((elem.offsetWidth - elem.parentNode.offsetWidth) / 2) + 'px';
+});
